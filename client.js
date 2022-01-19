7695c6ac2c9c0ee7c558d782041ae2cb0346489f
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-19 15:22:10 -0500

    added name to snek

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  conn.on("data", (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.on("connect", () => {
+    conn.write('Name: jae');
+    console.log('hi');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
